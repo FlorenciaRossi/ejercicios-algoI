90d26f1497631e47e9c1a59daa3d10cbe701d93e
FlorenciaRossi <flrossi@estudiantes.unsam.edu.ar>
2024-04-19 14:13:12 -0300

    comienzo ejercicio hospital ej33

--- hospital/src/App.java
+++ hospital/src/App.java
@@ -1,6 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
         Persona pepa = new Paciente("Pepa");
         System.out.println(pepa.getNombre());
     }
